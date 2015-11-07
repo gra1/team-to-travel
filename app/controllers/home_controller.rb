3054fe0600c97d143013aee7d4fdd89d51997368
yuzhva <yuzhva@debian.debian>
2015-11-07 09:15:03 +0200

    Chenged main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+
+  add_flash_types :success
+  add_flash_types :notice
+
+  respond_to :html,  :json
+  
+  add_flash_types :alert
+
   def index
+    flash[:success] = "Our congratulation! You just loaded main page!"
   end
 end
